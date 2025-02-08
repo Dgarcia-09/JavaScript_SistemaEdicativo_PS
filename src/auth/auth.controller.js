8764a6c252bf7ce2987a6477e21f50521bd5ddb1
Dgarcia-09 <dgarcia-2023532@kinal.edu.gt>
2025-02-07 21:27:12 -0600

    Add: carga del proyecto al repositorio

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,26 @@
+import { hash } from "argon2"
+import User from "../user/user.model.js"
+
+
+export const register = async (req, res) => {
+    try {
+        const data = req.body;
+        let profilePicture = req.file ? req.file.filename : null;
+        const encryptedPassword = await hash(data.password)
+        data.password = encryptedPassword
+        data.profilePicture = profilePicture
+
+        const user = await User.create(data);
+
+        return res.status(201).json({
+            message: "User has been created",
+            name: user.name,
+            email: user.email
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "User registration failed",
+            error: err.message
+        });
+    }
+}
